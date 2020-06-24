f92c792236b965701431ac27524757caaf8097b2
陈宇 <chenyu.cy41@bytedance.com>
2020-06-24 15:36:51 +0800

    cy commit hello world

--- app/src/main/java/com/example/caiyunmvvmdemo/donothing.kt
+++ app/src/main/java/com/example/caiyunmvvmdemo/donothing.kt
@@ -1,14 +1,7 @@
 package com.example.caiyunmvvmdemo
 
 fun main(){
-    val list= mutableListOf<Int>()
-
-    for(i in 1..5) list.add(i)
-
-    var x=0
-    for(i in 1..6) x=x xor i
-
-    println(list.find { it==5 })
+    println("hello world")
 
 
 
